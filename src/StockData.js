f27450f3a9368cfbed9148763ac5eb388ef97277
Ryan Manchester <ryanm17@gmail.com>
2024-02-22 09:43:37 -0500

    handles loading state of stock component

--- src/StockData.js
+++ src/StockData.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const StockData = () => {
   const [stockData, setStockData] = useState(null);
-  const[loading, setLoading] = useState(null);
+  const[loading, setLoading] = useState(true);
   const[error, setError] = useState(null);
 
   useEffect(() => {
@@ -22,20 +22,46 @@ const StockData = () => {
     fetchData();
   }, []);
 
-stockData.map((stock, index) => console.log(index));
-
-
+  if(loading) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div>
       <h2>Stock Data</h2>
-      <ul>
-        {stockData.map((stock, index) => (
-          <li key={index}>
-            <strong>Symbol</strong>{stock["01. symbol"]}<br />
-          </li>
-        ))}
-      </ul>
+      <table>
+        <thead>
+          <tr>
+            <th>Symbol</th>
+            <th>Open</th>
+            <th>High</th>
+            <th>Low</th>
+            <th>Price</th>
+            <th>Volume</th>
+            <th>Previous Close</th>
+            <th>Change</th>
+            <th>Change Percent</th>
+            <th>Latest Trading Day</th>
+          </tr>
+        </thead>
+        <tbody>
+          {stockData.map((stock, index) => (
+            <tr key={index}>
+              <td>{stock["01. symbol"]}</td>
+              <td>{stock["02. open"]}</td>
+              <td>{stock["03. high"]}</td>
+              <td>{stock["04. low"]}</td>
+              <td>{stock["05. price"]}</td>
+              <td>{stock["06. volume"]}</td>
+              <td>{stock["08. previous close"]}</td>
+              <td>{stock["09. change"]}</td>
+              <td>{stock["10. change percent"]}</td>
+              <td>{stock["07. latest trading day"]}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
     </div>
   );
 };
