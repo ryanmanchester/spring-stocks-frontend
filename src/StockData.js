95298c13337801f6da9fd1eafa3dafcafe454d63
Ryan Manchester <ryanm17@gmail.com>
2024-02-21 22:12:13 -0500

    mocking up react component to display stock data

---
+++ src/StockData.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+
+
+const StockData = () => {
+
+  return (
+    <div>
+      <h1>My Stonks</h1>
+    </div>
+  );
+};
+
+export default StockData;
